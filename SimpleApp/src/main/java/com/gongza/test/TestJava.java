a77f0d44f4a8df605a8596a44e4037a87e2f9bd5
gongza <fengyi036@163.com>
2017-04-08 09:17:09 +0800

    test

---
+++ SimpleApp/src/main/java/com/gongza/test/TestJava.java
@@ -0,0 +1,11 @@
+package com.gongza.test;
+
+/**
+ * Created by gongza on 2017/3/25.
+ */
+
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
