49696afa8501e2d127e13b8ac92ab2e8724b9da1
gongza <https://github.com/gongza036/SimpleGs.git>
2017-10-20 16:44:30 +0800

    ConstraintLayout，test

---
+++ SimpleApp/src/main/java/com/gongza/simplegs/activity/layout/ActivityConstraintLayout.java
@@ -0,0 +1,19 @@
+package com.gongza.simplegs.activity.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.gongza.simplegs.R;
+
+/**
+ * Created by gongza on 2017/9/20.
+ */
+
+public class ActivityConstraintLayout extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraintlayout);
+
+    }
+}
